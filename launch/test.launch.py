df0950957e666e0529987aaf7826b7a89a1233f0
Haruki Haneo <s23C1111TB@s.chibakoudai.jp>
2025-01-05 03:12:47 +0900

    testの編集

--- launch/test.launch.py
+++ launch/test.launch.py
@@ -6,8 +6,8 @@ import launch_ros.actions
 
 def generate_launch_description():
 
-    weatherpublisher = launch_ros.actions.Node(
+    weatherpublish = launch_ros.actions.Node(
         package='mypkg',      #パッケージの名前を指定
         executable='weatherpublisher',  #実行するファイルの指定
         )
-    return launch.LaunchDescription([weatherpublisher])                      
+    return launch.LaunchDescription([weatherpublish])                      
