fb14fd95f9a9923d433ce4f87a0a7e532ce67731
Haruki Haneo <s23C1111TB@s.chibakoudai.jp>
2025-01-05 15:32:23 +0900

    launch.pyの名前変更

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg test.launch.py > /tmp/mypkg.log
+timeout 15 ros2 launch mypkg weatherpublisher.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep '湿度'
+##cat /tmp/mypkg.log |
+grep '湿度:' /tmp/mypkg.log
