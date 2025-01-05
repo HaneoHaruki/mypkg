c5cb79f0954e6259fb1fe53a9d7d0b6d8e2c92cb
Haruki Haneo <s23C1111TB@s.chibakoudai.jp>
2025-01-05 15:46:58 +0900

    testの変更

--- test/test.bash
+++ test/test.bash
@@ -5,8 +5,8 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
-timeout 15 ros2 launch mypkg weatherpublisher.launch.py > /tmp/mypkg.log
+source $dir/ros2_ws/install/setup.bash
+timeout 30 ros2 launch mypkg weatherpublisher.launch.py > /tmp/mypkg.log
 
 ##cat /tmp/mypkg.log |
 grep '湿度:' /tmp/mypkg.log
